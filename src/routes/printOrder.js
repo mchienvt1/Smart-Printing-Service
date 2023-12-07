91eca57c5a9695375e9377542a9d192a8d2f0aaf
DESKTOP-NBOFPMA\lmchi <chien.leminh@hcmut.edu.vn>
2023-12-07 11:59:06 +0700

    complete

--- src/routes/printOrder.js
+++ src/routes/printOrder.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const printOrderController = require('../app/controllers/PrintOrderController');
+const printerController = require('../app/controllers/PrinterController');
+
 
 router.get('/modify', printOrderController.modify);
 router.post('/store', printOrderController.store);
+router.post('/searchPrinter',printerController.index);
 
 module.exports = router;
--- src/routes/printer.js
+++ src/routes/printer.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const printerController = require('../app/controllers/PrinterController');
+//const printerController = require('../app/controllers/PrinterController');
 
-router.get('/', printerController.index);
+//router.get('/', printerController.index);
 
-router.get('/:slugPrinter', printerController.printlist);
+//router.get('/:slugPrinter', printerController.printlist);
 
 module.exports = router;
